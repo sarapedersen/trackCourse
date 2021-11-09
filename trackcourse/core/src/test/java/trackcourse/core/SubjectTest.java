17f6ed56a1a433d11461525d8d0f119ebbfea494
Sara Pedersen <saraped@stud.ntnu.no>
2021-11-09 14:09:47 +0100

    added BeforeEach function

--- trackcourse/core/src/test/java/trackcourse/core/SubjectTest.java
+++ trackcourse/core/src/test/java/trackcourse/core/SubjectTest.java
@@ -1,92 +1,81 @@
 package trackcourse.core;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import trackcourse.core.Subject;
 
 public class SubjectTest {
+    private Subject testSubject;
+    private String testCode = "IT1901";
+
+    @BeforeEach
+    public void setup() {
+        testSubject = new Subject(testCode);
+    }
 
     @Test
     public void testConstructor() {
-        Subject sub = new Subject("ITGK");
-        Assertions.assertEquals(sub.getName(), "ITGK");
-        Assertions.assertEquals(sub.getDifficulty(), 0.0);
-        Assertions.assertEquals(sub.getEntertainment(), 0.0);
-        Assertions.assertEquals(sub.getTimeconsumption(), 0.0);
-        Assertions.assertEquals(sub.getNumDifficulty(), 0);
-        Assertions.assertEquals(sub.getNumEntertainment(), 0);
-        Assertions.assertEquals(sub.getNumTimeconsumption(), 0);
+        testSubject = new Subject("TDT4106");
+        Assertions.assertEquals(testSubject.getName(), "TDT4106");
+        Assertions.assertEquals(testSubject.getDifficulty(), 0.0);
+        Assertions.assertEquals(testSubject.getEntertainment(), 0.0);
+        Assertions.assertEquals(testSubject.getTimeconsumption(), 0.0);
+        Assertions.assertEquals(testSubject.getNumDifficulty(), 0);
+        Assertions.assertEquals(testSubject.getNumEntertainment(), 0);
+        Assertions.assertEquals(testSubject.getNumTimeconsumption(), 0);
     }
     
+    @Test
+    public void testSetName(){
+        testSubject.setName("IT2805");
+        Assertions.assertFalse(testSubject.getName() == testCode);
+        Assertions.assertEquals(testSubject.getName(), "IT2805");
+        
+    }
     
     @Test
     public void testUpdateFunctions() {
-    Subject sub = new Subject("Math");
-    Assertions.assertEquals(sub.getName(), "Math");
-    sub.updateDifficulty(5);
-    Assertions.assertEquals(sub.getDifficulty(), 5);
-    sub.updateDifficulty(6);
-    Assertions.assertFalse(sub.getDifficulty() == 5);
-    sub.updateTimeconsumption(3);
-    Assertions.assertEquals(sub.getTimeconsumption(), 3);
-    sub.updateEntertainment(3);
-    Assertions.assertEquals(sub.getEntertainment(), 3);
+    testSubject.updateDifficulty(5);
+    Assertions.assertEquals(testSubject.getDifficulty(), 5);
+    testSubject.updateDifficulty(6);
+    Assertions.assertFalse(testSubject.getDifficulty() == 5);
+    testSubject.updateTimeconsumption(3);
+    Assertions.assertEquals(testSubject.getTimeconsumption(), 3);
+    testSubject.updateEntertainment(3);
+    Assertions.assertEquals(testSubject.getEntertainment(), 3);
     
-    }
-
-    
-
-    @Test
-    public void testSetName(){
-
-        Subject sub = new Subject("Math");
-        sub.setName("ALGDAT");
-        Assertions.assertFalse(sub.getName() == "Math");
-        Assertions.assertEquals(sub.getName(), "ALGDAT");
-        
     }
 
     @Test
     public void testSetDiff(){
-
-        Subject sub = new Subject("Math");
-        sub.setDifficulty(3);
-        Assertions.assertEquals(sub.getDifficulty(), 3);
+        testSubject.setDifficulty(3);
+        Assertions.assertEquals(testSubject.getDifficulty(), 3);
         
     }
 
     @Test
     public void testSetTime(){
-
-        Subject sub = new Subject("Math");
-        sub.setTimeconsumption(5);
-        Assertions.assertEquals(sub.getTimeconsumption(), 5);
+        testSubject.setTimeconsumption(5);
+        Assertions.assertEquals(testSubject.getTimeconsumption(), 5);
 
     }
 
     @Test
     public void testSetEnt(){
-
-        Subject sub = new Subject("Math");
-        sub.setEntertainment(9);
-        Assertions.assertEquals(sub.getEntertainment(), 9);
+        testSubject.setEntertainment(9);
+        Assertions.assertEquals(testSubject.getEntertainment(), 9);
 
     }
 
     @Test
     public void testAverage(){
-        Subject sub = new Subject("IT1901");
-    
-        sub.updateDifficulty(16);
-        sub.updateTimeconsumption(15);
-        sub.updateEntertainment(20);
-        Assertions.assertEquals(sub.average(), 17);
+        testSubject.updateDifficulty(16);
+        testSubject.updateTimeconsumption(15);
+        testSubject.updateEntertainment(20);
+        Assertions.assertEquals(testSubject.average(), 17);
 
     }
-
-
-    
-
 }
 
