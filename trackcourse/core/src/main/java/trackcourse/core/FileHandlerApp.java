21eb7083fcbb9faff72aaabe8be0aea751cc2dc9
Gerhard <gerhargu@stud.ntnu.no>
2021-11-23 17:52:23 +0100

    made null apear on screen :)

--- trackcourse/core/src/main/java/trackcourse/core/FileHandlerApp.java
+++ trackcourse/core/src/main/java/trackcourse/core/FileHandlerApp.java
@@ -3,6 +3,9 @@ package trackcourse.core;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpRequest;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Scanner;
@@ -19,7 +22,13 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
-import trackcourse.springserver.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
 
 public class FileHandlerApp {
 
@@ -37,10 +46,29 @@ public class FileHandlerApp {
     this.subjects = subs;
   }
 
-  public void writeToJson(Collection<Subject> subs) throws JsonProcessingException, IOException {
+  public void writeToJson(Collection<Subject> subs) throws JsonProcessingException, IOException, URISyntaxException {
     for (Subject sub : subs) {
       ObjectMapper objectMapper = new ObjectMapper();
-      objectMapper.writeValue(new FileOutputStream("../core/src/json/" + sub.getFullName() + ".json"), sub);
+      objectMapper.writeValue(new FileOutputStream("../core/src/json/" + sub.getCourseCode() + ".json"), sub);
+    }
+
+    System.out.println("trying to post to server...");
+    Post("Saved");
+    System.out.println("done sending.");
+  }
+
+  public boolean Post(String str) throws URISyntaxException {
+    URI newUri = new URI("http://localhost:8080/data");
+    System.out.println("Strarting to send http");
+    try {
+      final HttpRequest req = HttpRequest.newBuilder(newUri).header("Accept", "application/json")
+          .header("Content-Type", "application/json").POST(BodyPublishers.ofString(str)).build();
+      final HttpResponse<String> res = HttpClient.newBuilder().build().send(req, HttpResponse.BodyHandlers.ofString());
+      System.out.println("Sycsessfully sendt http post");
+      return true;
+    } catch (IOException | InterruptedException e) {
+      System.out.println("Http request failed");
+      throw new RuntimeException(e);
     }
 
   }
