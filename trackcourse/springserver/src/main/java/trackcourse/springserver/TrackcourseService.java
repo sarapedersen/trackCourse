ed119c2d94768008ab0c1c5098fd343ce98bbf34
Steffen Hæren <steffenhaeren@gmail.com>
2021-11-23 11:55:35 +0100

    added check name method

--- trackcourse/springserver/src/main/java/trackcourse/springserver/TrackcourseService.java
+++ trackcourse/springserver/src/main/java/trackcourse/springserver/TrackcourseService.java
@@ -2,7 +2,9 @@ package trackcourse.springserver;
 
 import trackcourse.core.FileHandlerApp;
 import trackcourse.core.Subject;
+import trackcourse.core.CourseList;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.logging.FileHandler;
 
@@ -14,14 +16,33 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Service
 public class TrackcourseService {
 
   @GetMapping("/checkName")
-  public String checkName(String name) {
-    return "Hello World";
+  @ResponseBody
+  public String checkName(@RequestParam String name) throws IOException {
+    String response;
+
+    Subject sub = new Subject(name);
+    //CourseList courseList = new CourseList();
+    CourseList.getFullName(sub.getCourseCode());
+
+    if(CourseList.validate(name)){
+        response = "NAME IS VALID";
+    } else{
+        response = "NAME IS INVALID";
+    }
+
+    return "<html><title>Name check for" + sub.getCourseCode() + "</title>"
+            + "<body><h1>" + response + "</h1></body></html>";
+    
   }
+
+
 
 }
