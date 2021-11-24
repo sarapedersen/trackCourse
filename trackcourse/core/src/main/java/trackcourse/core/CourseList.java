3c68f2d644af98344476d21fb2ee1435b16ac1da
BuildTools <unconfigured@null.spigotmc.org>
2021-11-24 17:45:22 +0100

    Comments on CourseList class

--- trackcourse/core/src/main/java/trackcourse/core/CourseList.java
+++ trackcourse/core/src/main/java/trackcourse/core/CourseList.java
@@ -17,10 +17,11 @@ import java.util.List;
 
 public class CourseList {
 
+    //Once initialized (updateMap), this hashmap will contain all the courses avaiable on NTNU, and their respective coursecodes.
     final static Map<String, String> courseList = new HashMap<>();
     
 
-
+    //The validate function checks whether or not a given coursecode exists in the database (hashmap). 
     public static boolean validate(String course) throws IOException {
        try {
             if (getFullName(course).equals(null)) {
@@ -41,7 +42,11 @@ public class CourseList {
         }
 
     private static void updateMap() throws IOException {
-         
+         //This code might seem a little confusing at first, but after looking at the courseList.txt file it should be clear.
+
+         //The courseList.txt file is the raw output from a python script that iterated through NTNTus webpage to retrieve coursenames and codes.
+         //It is written like this:   COURSECODE /n COURSENAME  and so on.
+         //The for loop below simply iterates over it and creates a hashmap from the file
         if (courseList.isEmpty()) {
             Path path = Paths.get("../core/src/main/java/trackcourse/core/courseList.txt");
 	        List<String> lines = Files.readAllLines(path);
@@ -66,7 +71,6 @@ public class CourseList {
                     courseList.put(putcode, putname);
                 }
             }
-            //System.out.println(courseList);
 
         }
     }
